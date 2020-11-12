c0ec3f42346a131a868670e648efbbd71dd6b10f
Yudhiesh Ravindranath <yudhiesh1997@gmail.com>
2020-11-12 22:27:48 +0800

    ALL DONE

--- src/Form.tsx
+++ src/Form.tsx
@@ -8,19 +8,180 @@ import ForgotPassword from "./ForgotPassword";
 import ForgotPasswordSubmit from "./ForgotPasswordSubmit";
 
 export type TInputEvent = React.ChangeEvent<HTMLInputElement>;
+type TUpdateFormType = (formType: string) => void;
 
-interface IIntialState {
+export interface IIntialState {
   username: string;
   password: string;
   email: string;
+  phone_number: string;
   confirmationCode: string;
+}
+
+export interface IUserState {
+  email: string;
+  email_verified: boolean;
+  sub: string;
+  username: string;
 }
 
 const initialState: IIntialState = {
   username: "",
   password: "",
   email: "",
+  phone_number: "",
   confirmationCode: ""
+};
+
+async function signUp(
+  { username, password, email }: IIntialState,
+  updateFormType: TUpdateFormType
+) {
+  try {
+    await Auth.signUp({ username, password, attributes: { email } });
+    console.log("Sign Up was a success");
+    updateFormType("confirmSignUp");
+  } catch (error) {
+    console.log("Error while signing in!", error);
+  }
+}
+async function confirmSignUp(
+  { username, confirmationCode }: IIntialState,
+  updateFormType: TUpdateFormType
+) {
+  try {
+    await Auth.confirmSignUp(username, confirmationCode);
+    console.log("Sign in was confirmed");
+    updateFormType("signIn");
+  } catch (error) {
+    console.log("Error while confirming sign up!", error);
+  }
+}
+async function signIn(
+  { username, password }: IIntialState,
+  setUser: (user: React.SetStateAction<IUserState | null>) => void
+) {
+  try {
+    const user = await Auth.signIn(username, password);
+    const userInfo = { username: user.username, ...user.attributes };
+    setUser(userInfo);
+  } catch (error) {
+    console.log("Unable to sign in!", error);
+  }
+}
+async function forgotPassword(
+  { username }: IIntialState,
+  updateFormType: TUpdateFormType
+) {
+  try {
+    await Auth.forgotPassword(username);
+    updateFormType("forgotPasswordSubmit");
+  } catch (error) {
+    console.log("Error resetting the password", error);
+  }
+}
+async function forgotPasswordSubmit(
+  { username, confirmationCode, password }: IIntialState,
+  updateFormType: TUpdateFormType
+) {
+  try {
+    await Auth.forgotPasswordSubmit(username, confirmationCode, password);
+    updateFormType("signIn");
+  } catch (error) {
+    console.log("Error while updating password", error);
+  }
+}
+
+const Form = (props: any) => {
+  const [formType, updateFormType] = useState("signIn");
+  const [formState, updateFormState] = useState(initialState);
+  const updateForm = (event: TEvent) => {
+    const target = event.target as HTMLInputElement;
+    const newFormState = {
+      ...formState,
+      [target.name]: target.value
+    };
+    updateFormState(newFormState);
+  };
+
+  function renderForm() {
+    switch (formType) {
+      case "signUp":
+        return (
+          <SignUp
+            signUp={() => signUp(formState, updateFormType)}
+            updateFormState={(e: TEvent) => updateForm(e)}
+          />
+        );
+      case "confirmSignUp":
+        return (
+          <ConfirmSignUp
+            confirmSignUp={() => confirmSignUp(formState, updateFormType)}
+            updateFormState={(e: TEvent) => updateForm(e)}
+          />
+        );
+      case "signIn":
+        return (
+          <SignIn
+            signIn={() => signIn(formState, props?.setUser)}
+            updateFormState={(e: TEvent) => updateForm(e)}
+          />
+        );
+      case "forgotPassword":
+        return (
+          <ForgotPassword
+            forgotPassword={() => forgotPassword(formState, updateFormType)}
+            updateFormState={(e: TEvent) => updateForm(e)}
+          />
+        );
+      case "forgotPasswordSubmit":
+        return (
+          <ForgotPasswordSubmit
+            forgotPasswordSubmit={() =>
+              forgotPasswordSubmit(formState, updateFormType)
+            }
+            updateFormState={(e: TEvent) => updateForm(e)}
+          />
+        );
+      default:
+        return null;
+    }
+  }
+  return (
+    <div>
+      {renderForm()}
+      {formType === "signUp" && (
+        <p style={styles.toggleForm}>
+          Already have an account?{" "}
+          <span style={styles.anchor} onClick={() => updateFormType("signIn")}>
+            Sign In
+          </span>
+        </p>
+      )}
+      {formType === "signIn" && (
+        <>
+          <p style={styles.toggleForm}>
+            Need an account?{" "}
+            <span
+              style={styles.anchor}
+              onClick={() => updateFormType("signUp")}
+            >
+              Sign Up
+            </span>
+          </p>
+          <p style={{ ...styles.toggleForm, ...styles.resetPassword }}>
+            Forget your password?{" "}
+            <span
+              style={styles.anchor}
+              onClick={() => updateFormType("forgotPassword")}
+            >
+              Reset Password
+            </span>
+          </p>
+        </>
+      )}
+    </div>
+  );
 };
 
 const styles: IStyles = {
@@ -57,82 +218,6 @@ const styles: IStyles = {
     color: "#006bfc",
     cursor: "pointer"
   }
-};
-
-type TUpdateFormType = (formType: string) => void;
-
-async function signUp(
-  { username, password, email }: IIntialState,
-  updateFormType: TUpdateFormType
-) {
-  try {
-    await Auth.signUp({ username, password, attributes: { email } });
-    console.log("Sign Up was a success");
-    updateFormType("confirmSignUp");
-  } catch (error) {
-    console.log("Error while signing in!", error);
-  }
-}
-async function confirmSignUp(
-  { username, confirmationCode }: IIntialState,
-  updateFormType: TUpdateFormType
-) {
-  try {
-    await Auth.confirmSignUp(username, confirmationCode);
-    console.log("Sign in was confirmed");
-    updateFormType("signIn");
-  } catch (error) {
-    console.log("Error while confirming sign up!", error);
-  }
-}
-async function signIn({ username, password }: IIntialState, setUser: any) {
-  try {
-    const user = await Auth.signIn(username, password);
-    const userInfo = { username: user.username, ...user.attributes };
-    setUser(userInfo);
-  } catch (error) {
-    console.log("Unable to sign in!", error);
-  }
-}
-async function forgotPassword(
-  { username }: IIntialState,
-  updateFormType: TUpdateFormType
-) {
-  try {
-    await Auth.forgotPassword(username);
-    updateFormType("forgotPasswordSubmit");
-  } catch (error) {
-    console.log("Error resetting the password", error);
-  }
-}
-async function forgotPasswordSubmit(
-  { username, confirmationCode, password }: IIntialState,
-  updateFormType: TUpdateFormType
-) {
-  try {
-    await Auth.forgotPasswordSubmit(username, confirmationCode, password);
-    updateFormType("signIn");
-  } catch (error) {
-    console.log("Error while updating password", error);
-  }
-}
-
-const Form = (props: React.ReactNode) => {
-  const [formType, updateFormType] = useState("signIn");
-  const [formState, updateFormState] = useState(initialState);
-
-  const updateForm = (event: TInputEvent) => {
-    const newFormState = {
-      ...formState,
-      [event.target.name]: event.target.value
-    };
-    updateFormState(newFormState);
-  };
-
-  function renderForm() {
-    return <div>Hello</div>;
-  }
-  return <div>{renderForm()}</div>;
 };
 
 export { styles, Form as default };
--- src/Profile.tsx
+++ src/Profile.tsx
@@ -1,37 +1,47 @@
 import React, { useState, useEffect } from "react";
-import { Auth } from "aws-amplify";
-import { withAuthenticator, AmplifySignOut } from "@aws-amplify/ui-react";
+import { Button } from "antd";
+import { Auth, Hub } from "aws-amplify";
 import Container from "./Container";
+import Form, { IIntialState } from "./Form";
 
-interface User {
-  username: string;
-  email: string;
-  phone_number: string;
-}
+function Profile() {
+  const [user, setUser] = useState<IIntialState | null>(null);
 
-const Profile = () => {
-  const [user, setUser] = useState({} as User);
   useEffect(() => {
     checkUser();
+    Hub.listen("auth", data => {
+      const { payload } = data;
+      if (payload.event === "signOut") {
+        setUser(null);
+      }
+    });
   }, []);
 
-  const checkUser = async () => {
+  async function checkUser() {
     try {
       const data = await Auth.currentUserPoolUser();
       const userInfo = { username: data.username, ...data.attributes };
+      console.log(userInfo);
       setUser(userInfo);
-    } catch (error) {
-      console.log(error);
+    } catch (err) {
+      console.log("error: ", err);
     }
-  };
-  return (
-    <Container>
-      <h1>Profile</h1>
-      <h2>Username: {user.username}</h2>
-      <h2>Email: {user.email}</h2>
-      <h2>Phone: {user.phone_number}</h2>
-      <AmplifySignOut />
-    </Container>
-  );
-};
-export default withAuthenticator(Profile);
+  }
+  function signOut() {
+    Auth.signOut().catch(err => console.log("error signing out: ", err));
+  }
+  if (user) {
+    return (
+      <Container>
+        <h1>Profile</h1>
+        <h2>Username: {user.username}</h2>
+        <h3>Email: {user.email}</h3>
+        <h4>Phone: {user.phone_number}</h4>
+        <Button onClick={signOut}>Sign Out</Button>
+      </Container>
+    );
+  }
+  return <Form setUser={setUser} />;
+}
+
+export default Profile;
